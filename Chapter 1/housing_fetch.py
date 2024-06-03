93701843b322ef0e8fccb6a46feac376d2684143
kwundram2602 <134778951+kwundram2602@users.noreply.github.com>
2024-06-03 15:13:19 +0200

    1_1

---
+++ Chapter 1/housing_fetch.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import pandas as pd
+import tarfile
+import urllib.request
+import matplotlib.pyplot as plt
+def load_housing_data ():
+    tarball_path = Path ("datasets/housing.tgz" )
+    if not tarball_path.is_file ():
+        Path ("datasets" ).mkdir (parents =True , exist_ok =True )
+        url = "https://github.com/ageron/data/raw/main/housing.tgz"
+        urllib .request.urlretrieve (url , tarball_path )
+        with tarfile .open (tarball_path ) as housing_tarball :
+            housing_tarball .extractall (path ="datasets" )
+    return pd.read_csv (Path ("datasets/housing/housing.csv" ))
+housing = load_housing_data ()
+#housing.head()
+#housing.info ()
+#print(housing["ocean_proximity" ].value_counts())
+print(housing.describe())
+housing.hist (bins =50, figsize =(12, 8))
+plt.show ()
